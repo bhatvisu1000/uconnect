cbc66b70c246406250a3fb93cedbaacf2481e6b7
myuconnect <oracledba.usa88@gmail.com>
2017-05-08 21:43:25 -0400

    remove ucexcetion reference

--- myapp/src/com/uconnect/bps/__init__.py
+++
@@ -1,4 +0,0 @@
-from com.uconnect.core.ucexception import MissingConfigFile
-from com.uconnect.core.ucexception import MissingArgumentValue
-from com.uconnect.core.ucexception import BootStrapError
-from com.uconnect.core.ucexception import MyError
